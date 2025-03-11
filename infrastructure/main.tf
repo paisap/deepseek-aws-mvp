ac293c6a54e1815e1f1825050f01d89d62480d60
root <root@DESKTOP-16HI4U1.localdomain>
2025-03-11 06:24:20 -0500

    prueba deploy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 resource "aws_instance" "deepseek_server" {
   ami                    = "ami-0884d2865dbe9de4b"  # Ubuntu 22.04 LTS
   instance_type          = "g4dn.xlarge"            # GPU NVIDIA T4
-  key_name               = "github-actions-key"            # Nombre de tu clave SSH en AWS
+  key_name               = "R1"            # Nombre de tu clave SSH en AWS
   vpc_security_group_ids = [aws_security_group.deepseek_sg.id]
 
   tags = {
