c9aa73031ea5f04b8f1525b46a734c1c47fff7ef
Paisap <santiago@gmail.com>
2025-05-12 18:10:27 -0500

    MVP funcional mi papa

--- app/api/main.py
+++ app/api/main.py
@@ -1,36 +1,70 @@
 from fastapi import FastAPI, UploadFile, File
+from langchain_community.llms import Ollama
+from rag.embeddings import process_documents, load_vector_db
 import httpx
 
 
 app = FastAPI()
 OLLAMA_URL = "http://ollama:11434/api/generate"
 
+
+
+llm = Ollama(
+    base_url="http://ollama:11434",  # Nombre del servicio en docker-compose
+    model="deepseek-r1:14b"
+)
+
 @app.post("/presentar")
-async def presentar(nombre: str):
+async def presentar(pregunta: str, usar_contexto: bool = False):
+    print(pregunta)
+    print("---------------")
+    print("soy el contexto")
+    print(usar_contexto)
+
+    if usar_contexto:
+        vector_db = load_vector_db()
+        context = vector_db.similarity_search(pregunta, k=3)
+        prompt = f"""
+        
+        {context}
+
+        Pregunta: {pregunta}
+        """
+        print("SOY  LA PREGUNTA CON CONTEXTO")
+        print(prompt)
+    else:
+        prompt = f"""
+        {pregunta}
+        """
+        print("SOY  LA PREGUNTA sin CONTEXTO")
+        print(prompt)
     # 1. Buscar en RAG
-    from rag.embeddings import process_documents
-    vector_db = process_documents()
-    context = vector_db.similarity_search(nombre, k=3)
+
+    # vector_db = load_vector_db()
+    # context = vector_db.similarity_search(nombre, k=3)
     
-    # 2. Consultar a Ollama
-    prompt = f"""
-    Genera una presentación profesional sobre {nombre} usando este contexto:
-    {context}
-    """
-    
-    async with httpx.AsyncClient() as client:
+    # # 2. Consultar a Ollama
+    # prompt = f"""
+    # Genera una presentación profesional sobre {nombre} usando este contexto:
+    # {context}
+    # """
+    # print("SOY EL CONTEXTOOOOO")
+    # print(context)
+    async with httpx.AsyncClient(timeout=httpx.Timeout(300.0)) as client:
         response = await client.post(
             OLLAMA_URL,
             json={"model": "deepseek-r1:14b", "prompt": prompt, "stream": False}
         )
     
     return {"presentacion": response.json()["response"]}
+    #return {"presentacion": "holas"}
 
 
 @app.post("/upload")
 async def upload_persona(file: UploadFile = File(...)):
     # Guardar el archivo subido
-    file_path = f"app/rag/data/{file.filename}"
+    print("hola")
+    file_path = f"/app/rag/data/{file.filename}"
     with open(file_path, "wb") as f:
         f.write(await file.read())
     
--- app/rag/embeddings.py
+++ app/rag/embeddings.py
@@ -1,13 +1,18 @@
 from langchain.document_loaders import TextLoader  # Cambio clave
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 def process_documents():
     # Cargar desde .txt
-    loaders = [TextLoader(f"app/rag/data/{name}.txt") for name in ["persona1", "persona2"]]
+    loaders = [TextLoader(f"/app/rag/data/{name}.txt") for name in ["persona1", "persona2"]]
+
     
     docs = []
     for loader in loaders:
         docs.extend(loader.load())
+
     
     # Dividir en chunks
     text_splitter = RecursiveCharacterTextSplitter(
@@ -20,3 +25,10 @@ def process_documents():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
     vector_db = FAISS.from_documents(chunks, embeddings)
     vector_db.save_local("rag_db")
+    return vector_db
+
+
+def load_vector_db():
+    print("hola soy el load")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    return FAISS.load_local("/app/rag_db", embeddings, allow_dangerous_deserialization=True)
